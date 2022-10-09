7539daef747e4d316adca0b8905940b5df7bcce9
Will Worth <willworthdev@gmail.com>
2022-10-09 16:14:30 +0200

    whitelist and dtos

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { Report } from './reports/report.entity';
     type: 'sqlite',
     database: 'db.sqlite',
     entities:[User, Report],
-    synchronize: true
+    synchronize: true // automatically does migrations
   }),  
     UsersModule, ReportsModule],
   controllers: [AppController],
