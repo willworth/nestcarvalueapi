09ba690de7a3f32536081fe6539ff7f23aecfd78
Will Worth <willworthdev@gmail.com>
2022-10-10 14:41:35 +0200

    logger and hooks

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,4 +1,9 @@
-import { Entity,Column, PrimaryGeneratedColumn } from "typeorm";
+import { Logger } from '@nestjs/common';
+import {
+    AfterInsert,
+    AfterRemove,
+    AfterUpdate, Entity, Column, PrimaryGeneratedColumn
+} from "typeorm";
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
@@ -7,4 +12,19 @@ export class User {
     email: string;
     @Column()
     password: string;
+
+    // if you don't use the entity (just manually pass an object), hooks are NOT triggered
+    @AfterInsert()
+    logInsert() {
+        // Logger.log('Inserted file with ID', this.id);
+        Logger.log(`Inserted User with ID ${this.id}`);
+    }
+    @AfterUpdate()
+    logUpdate() {
+        Logger.log(`Updated User with ID ${this.id}`);
+    }
+    @AfterRemove()
+    logRemove() {
+        Logger.log(`Removed User with ID ${this.id}`);
+    }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,15 @@
 import {Body, Controller,Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
-
+    constructor(private usersService: UsersService){}
     @Post('/signup')
     createUser(@Body() body: CreateUserDto){
         console.log('body: ', body);
-        
+
+        this.usersService.create(body.email,body.password);
     }
 }
- 
+  
